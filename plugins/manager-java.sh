a8bc98e83abadcbace77078dda8ecd8687b0dad4
jefersonla <oi_je@hotmail.com>
2016-08-18 17:46:01 -0300

    added rule to download java

--- plugins/manager-java.sh
+++ plugins/manager-java.sh
@@ -1,5 +1,12 @@
 JAVA_VERSION="8u101"
 NB_VERSION="8_1"
+JAVA_PLATFORM="x64"
+# Installer FILE
+INSTALLER_NAME="jdk-""$JAVA_VERSION""-nb-""$NB_VERSION""-linux-$JAVA_PLATFORM.sh"
+INSTALLER="$(pwd)/bin/$INSTALLER_NAME"
+# Latest JDK8 version released on 19th July, 2016: JDK8u101
+BASE_URL="http://download.oracle.com/otn-pub/java/jdk-nb/""$JAVA_VERSION""-8.1/"
+JDK_NB_URL="$BASE_URL$INSTALLER_NAME"
 
 show_help(){
     printf "Manager for Java JDK with Netbeans Portable!\n"
@@ -8,9 +15,13 @@ show_help(){
     printf "\t./manager-jdk.sh [option]\n"
     printf "\nOptions:\n"
     printf "\tinstall\tInstall Java JDK\n" "$HOME"
+    printf "\tdownload\tDownload binaries for installation"
     printf "\thelp\tShow this help message\n"
 }
 
+download_app(){
+    wget -c -O "$(pwd)/bin/$INSTALLER_NAME" --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" "$JDK_NB_URL" || rm "$INSTALLER"
+}
 
 if [ $# -gt 1 ] || [ $# -eq 0 ];then
     show_help
@@ -19,7 +30,17 @@ fi
 
 case $1 in
     install)
-        "$(pwd)/jdk-""$JAVA_VERSION""-nb-""$NB_VERSION""-linux-x64.sh"
+        if [ ! -e "$(pwd)/bin" ] || [ ! -e "$(pwd)/bin/$INSTALLER_NAME" ];then
+            mkdir "$(pwd)/bin"
+            download_app
+        fi
+        "$INSTALLER"
+        ;;
+    download)
+        if [ ! -e "$(pwd)/bin" ] || [ ! -e "$(pwd)/bin/$INSTALLER_NAME" ];then
+            mkdir "$(pwd)/bin"
+            download_app
+        fi
         ;;
     help)
         show_help
